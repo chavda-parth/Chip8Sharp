280349d133a3f9c291eb32433695258567500a49
chavda-parth <132228922+chavda-parth@users.noreply.github.com>
2026-07-26 12:26:00 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Prologue: Another incomplete project maybe? Only time will tell.");
+Console.WriteLine("Let the commits commence.");
---
+++ README.md
@@ -0,0 +1,1 @@
+A Chip-8 interpreter/emulator in C#.
