d065f54eed47f3d93372402ed7a123f100f8420c
chavda-parth <132228922+chavda-parth@users.noreply.github.com>
2026-07-30 22:01:10 +0530

    Rename Machine.cs and add LoadRom method.

---
+++ Chip8.cs
@@ -0,0 +1,43 @@
+public class Chip8 {
+	private byte[] memory = new byte[4096]; // RAM
+	private byte[] registers = new byte[16]; // Register V0 to VF
+	private byte[] keypad = new byte[16];
+
+	private ushort[] executionStack = new ushort[16]; // Stack to jump to subroutines.
+	
+	private byte delayTimer; // Timer
+	private byte soundTimer; // Beep timer
+	private byte stackPointer; // Pointer pointing to a location on the execution stack.
+
+	private ushort index; // Register simply called I in docs.
+	private ushort pc; // Program Counter
+	private ushort opcode; 
+
+	private bool[] video = new bool[64 * 32];
+
+	private const ushort START_ADDRESS = 0x200;
+
+	public void LoadRom(string fileName)
+	{
+		// Check if rom file exists.
+		if (!File.Exists(fileName))
+		{
+			Console.WriteLine("Could not find rom.");
+			return;
+		}
+
+		// Read rom file as a byte array.
+		var rom = File.ReadAllBytes(fileName);
+
+		if (rom != null && rom.Length > 0)
+		{
+			Console.WriteLine($"Rom found successfully. Size: {rom.Length}");
+		}
+
+		// Load the rom into memory.
+		for (int i = 0; i < rom.Length; i++)
+		{
+			memory[START_ADDRESS + i] = rom[i];
+		}
+	} 
+}
--- Program.cs
+++ Program.cs
@@ -1,45 +1,7 @@
-﻿using Raylib_cs;
-
-public static class Program
+﻿public static class Program
 {
-    enum MovementDirection {
-        LTR,
-        RTL
-    }
-
     public static void Main() 
     {
-        Raylib.InitWindow(640, 320, "Testing Window");
-        Raylib.SetTargetFPS(60);
-        
-
-        float x = 0;
-        var moveDir = MovementDirection.LTR;
-        int speed = 100;
-
-        while (!Raylib.WindowShouldClose()) 
-        {
-            if (moveDir == MovementDirection.LTR && x >= 540)
-            {
-                moveDir = MovementDirection.RTL;
-            }
-            else if (moveDir == MovementDirection.RTL && x <= 0)
-            {
-                moveDir = MovementDirection.LTR;
-            }
-
-            x += (Raylib.GetFrameTime() * (moveDir == MovementDirection.LTR ? speed : -speed)); 
-
-            // Console.WriteLine($"x: {x}");
-
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.White);            
-
-            // Raylib.DrawText("Hello, World", 12, 12, 20, Color.Black);
-            Raylib.DrawRectangle((int) x, 12, 100, 100, Color.Black);
-            Raylib.EndDrawing();
-        }
-
-        Raylib.CloseWindow();
+       
     }
 }
